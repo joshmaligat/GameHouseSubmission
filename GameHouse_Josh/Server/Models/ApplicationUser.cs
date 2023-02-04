650cd429cb237bca436f2edc49b55ef0897a89fc
Admin <Admin@DESKTOP-7806A9F>
2023-02-04 18:18:56 +0800

    Add project files.

---
+++ GameHouse_Josh/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameHouse_Josh.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
